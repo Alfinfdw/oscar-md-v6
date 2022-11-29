c9ef85dcb563e1080f177b75e8c9b722058d024b
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2022-11-29 17:50:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,31 +14,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285850539404'] //ur owner number
-global.ownername = "Jarot Offc" //ur owner name
-global.developer = "Jarot Offc" //nama lu
-global.ytname = "YT: Jarot offc" //ur yt chanel name
-global.socialm = "https://instagram.com/jarotr_" //ur github or insta name
+global.vcardowner = ['4915256954581'] //ur owner number
+global.ownername = "NiluhUwuw" //ur owner name
+global.developer = "NiluhUwuw" //nama lu
+global.ytname = "YT: NiluhUwuw" //ur yt chanel name
+global.socialm = "https://instagram.com/bednocturnal_" //ur github or insta name
 global.sgc = "https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL"
 global.location = "Jatim" //ur location
 
 //bot bomdy 
-global.owner = ['6285850539404'] //ur number
-global.ownernomer = "6285850539404" //ur number
-global.ownertag = '6285850539404' //ur tag number
-global.premium = ['6285850539404'] //ur premium number
-global.botname = '𝘖𝘴𝘤𝘢𝘳-𝘔𝘥' //ur bot name
-global.ownername = "𝘑𝘢𝘳𝘰𝘵 𝘖𝘧𝘧𝘤"
+global.owner = ['4915256954581'] //ur number
+global.ownernomer = "4915256954581" //ur number
+global.ownertag = '4915256954581' //ur tag number
+global.premium = ['4915256954581'] //ur premium number
+global.botname = 'NiluhUwuw' //ur bot name
+global.ownername = "Niluhjatim"
 global.linkz = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //your theme url which will be displayed on whatsapp
-global.dana = "6285850539404"
-global.pulsa = "6285850539404"
+global.dana = "4915256954581"
+global.pulsa = "4915256954581"
 global.websitex = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur website to be displayed
 global.websitex1 = "https://youtube.com/shorts/N6cPd_V-IME?feature=share"
 global.botscript = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Oscar Bot" //ur sticker watermark author
-global.wm = "Jarot ofc" //ur watermark
+global.author = "Niluh Bot" //ur sticker watermark author
+global.wm = "NiluhUwuw" //ur watermark
 
 // Other
 global.sessionName = 'Session'
